ec379fe486a5106589d655e16206f6a75a30b676
Alex Stueve <amstueve@ksu.edu>
2020-01-29 15:09:00 -0600

    1/29 - 3 classes made so far

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Alex Stueve
+ * AngryChicken.cs
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// This class defines the Angry Chicken entree
+    /// </summary>
+    class AngryChicken
+    {
+        private bool bread = true;
+        /// <summary>
+        /// If the chicken is served with bread
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// If the chicken is topped with pickles
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// The price of the chicken
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.99;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the chicken
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 190;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the chicken
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold pickles");
+
+                return instructions;
+            }
+        }
+
+    }
+}
---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Alex Stueve
+ * RustlersRibs.cs
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class represented the Rustler's Ribs entree.
+    /// </summary>
+    class RustlersRibs
+    {
+        /// <summary>
+        /// The price of the ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the ribs
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                return new List<string>();
+            }
+        }
+
+    }
+}
