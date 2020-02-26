f647bf74c97eafe3a2e0d29dae4dfa942230650f
Alex Stueve <amstueve@ksu.edu>
2020-02-26 14:48:38 -0600

    2/26 - Order class and test files added

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public interface IOrderItem
+    {
+
+        /// <summary>
+        /// The price of this order item
+        /// </summary>
+        double Price { get; }
+
+        List<string> SpecialInstructions { get; }
+
+    }
+}
