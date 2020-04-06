41d598541afd60a33183d6b016536b644066478a
Alex Stueve <amstueve@ksu.edu>
2020-04-06 11:59:25 -0500

    4/6 - pay by credit working, need to rework lab stuff for cash

--- PointOfSale/TransactionControl.xaml.cs
+++ PointOfSale/TransactionControl.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CowboyCafe.Data;
+using CashRegister;
+using CowboyCafe.Extensions;
 
 namespace PointOfSale
 {
@@ -19,9 +21,44 @@ namespace PointOfSale
     /// </summary>
     public partial class TransactionControl : UserControl
     {
+
+        private static CashDrawer cashDrawer = new CashDrawer();
+
         public TransactionControl()
         {
             InitializeComponent();
         }
+
+        public void OnCreditButtonClicked(object sender, RoutedEventArgs e)
+        {
+            CardTerminal ct = new CardTerminal();
+            if (DataContext is Order order)
+            {
+                ResultCode rc = ct.ProcessTransaction(order.Total);
+                if (rc == ResultCode.Success)
+                {
+                    ReceiptPrinter rp = new ReceiptPrinter();
+                    rp.Print(order.Total.ToString());
+                    var orderControl = this.FindAncestor<OrderControl>();
+                    orderControl.SwapScreen(new MenuItemSelectionControl());
+                    orderControl.DataContext = new Order();
+                }
+                else
+                {
+                    MessageBox.Show($"{rc}");
+                }
+            }
+        }
+
+        public void OnCashButtonClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void OnCancelButtonClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
